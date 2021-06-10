bb831663a50da954aef06047fd465d7ba73e25f0
RMtzB <64701673+RMtzB@users.noreply.github.com>
2021-06-09 22:44:51 -0500

    Se agrego Swagger

--- src/main/java/mx/uam/ayd/proyecto/datos/UsuarioRepository.java
+++ src/main/java/mx/uam/ayd/proyecto/datos/UsuarioRepository.java
@@ -18,7 +18,5 @@ public interface UsuarioRepository extends CrudRepository <Usuario, Long> {
 	
 	public Usuario findByNombreAndApellido(String nombre, String apellido);
 	
-	public List <Usuario> findByEdadBetween(int edad1, int edad2);
-	
 	public Usuario findByIdUsuario(Long id);
 }
