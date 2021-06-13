3b0d7ee3c7601370c8261a6d5d18eaba1550edef
CCLOM <33014094+CCLOM@users.noreply.github.com>
2021-06-13 17:45:30 -0500

    PublicacionRestController Terminado

---
+++ src/main/java/mx/uam/ayd/proyecto/dto/comentarioDto.java
@@ -0,0 +1,15 @@
+package mx.uam.ayd.proyecto.dto;
+
+import lombok.Data;
+import mx.uam.ayd.proyecto.negocio.modelo.Publicacion;
+@Data
+public class comentarioDto {
+	private String comentarios;
+
+	public static comentarioDto creaDto(Publicacion p) {
+		comentarioDto dto = new comentarioDto();
+		dto.setComentarios(p.getComentarios());
+		return dto;
+	}
+
+}
